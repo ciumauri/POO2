88dd5843e59c52609d6f86a616d13d017cbf213d
Mauricio Oliveira <55008505+ciumauri@users.noreply.github.com>
2022-06-20 16:48:00 -0300

    Update

--- ex_aula7_CRUD/DaoVeiculo.java
+++ ex_aula7_CRUD/DaoVeiculo.java
@@ -8,53 +8,53 @@ public class DaoVeiculo {
     private Connection conn;
     private Statement st;
 
-    private void conectar(){
+    private void conectar() {
         try {
             this.conn = GerenciadorConexao.pegarConexao();
             st = conn.createStatement();
         } catch (ClassNotFoundException e1) {
             System.out.println(e1.getMessage());
-        }catch (SQLException e2) {
+        } catch (SQLException e2) {
             System.out.println(e2.getMessage());
         }
     }
 
-    private void desconectar(){
+    private void desconectar() {
         try {
             st.close();
             conn.close();
-        }catch (SQLException e) {
-            System.out.println("Erro ao fechar a conexao: " 
-                + e.getMessage());
-        }        
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar a conexao: "
+                    + e.getMessage());
+        }
     }
 
-    public boolean inserir(Veiculo v){
+    public boolean inserir(Veiculo v) {
         boolean resultado = false;
         try {
             this.conectar();
-			String comando = "INSERT INTO tb_veiculos VALUES (NULL, '"
-				+ v.getMarca() + "', '" + v.getModelo() + "', '"
-				+ v.getChassi() + "', " + v.getAno() + ");";
-            
-            //System.out.println(comando);
+            String comando = "INSERT INTO tb_veiculos VALUES (NULL, '"
+                    + v.getMarca() + "', '" + v.getModelo() + "', '"
+                    + v.getChassi() + "', " + v.getAno() + ");";
+
+            // System.out.println(comando);
             st.executeUpdate(comando);
             resultado = true;
         } catch (SQLException e) {
-            System.out.println("Erro ao inserir o registro: " 
-                + e.getMessage());
-        }finally{
+            System.out.println("Erro ao inserir o registro: "
+                    + e.getMessage());
+        } finally {
             this.desconectar();
         }
         return resultado;
     }
 
-    public ArrayList<Veiculo> buscarTodos(){
+    public ArrayList<Veiculo> buscarTodos() {
         ArrayList<Veiculo> resultados = new ArrayList<Veiculo>();
         try {
             this.conectar();
             ResultSet rs = st.executeQuery("select * from tb_veiculos order by modelo;");
-            while(rs.next()){
+            while (rs.next()) {
                 Veiculo v = new Veiculo();
                 v.setCodigo(rs.getInt("codigo"));
                 v.setMarca(rs.getString("marca"));
@@ -66,7 +66,7 @@ public class DaoVeiculo {
             }
         } catch (Exception e) {
             System.out.println("Erro..." + e.getMessage());
-        } finally{
+        } finally {
             this.desconectar();
         }
         return resultados;
@@ -81,19 +81,25 @@ public class DaoVeiculo {
             qtde = st.getUpdateCount();
         } catch (Exception e) {
             System.out.println("Erro... " + e.getMessage());
-        }finally{
+        } finally {
             this.desconectar();
         }
         return qtde;
     }
 
-    public int editar(int cod) {
+    public int editar(Veiculo v) {
         int qtde = 0;
         try {
             this.conectar();
-            String comando = "update from tb_veiculos where codigo = " + cod + ";";
+            String comando = "update tb_veiculos set marca = ?, modelo = ?, chassi = ?, ano = ?," + "where codigo = ?" + ";";
             st.executeUpdate(comando);
             qtde = st.getUpdateCount();
+            st.
+
+
+
+
+
         } catch (Exception e) {
             System.out.println("Erro... " + e.getMessage());
         }finally{
@@ -102,7 +108,4 @@ public class DaoVeiculo {
         return qtde;
     }
 
-    public boolean editar(Veiculo veiculo) {
-        return false;
-    }
 }
